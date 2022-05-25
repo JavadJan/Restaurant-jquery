ad78f31d43ac801a2eb6c9e44c69091304def0b9
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-05-25 22:02:31 +0300

    finished until plus icon

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
     
 })
 $(document).ready(function(){
+    let i=0;
     $("#slideDown").click(function(){
       $("#menu").show('slow');
     });
@@ -20,5 +21,14 @@ $(document).ready(function(){
     $("#menu").mouseleave(function(){
         $(this).hide("slow");
       });
+    
+      $('.uil-plus').click(function () {
+          $(this).prev().text((i+=1).toString())
+          
+      })
+      $('.uil-minus').click(function () {
+        $(this).prev().parseInt(text())-=1
+        
+    })
   });
 
