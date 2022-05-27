458a32b2ecb4b1cafddccf6728592e4846a3cbb7
JavadJan <92234079+JavadJan@users.noreply.github.com>
2022-05-27 18:38:50 +0300

    finished until slider

--- main.js
+++ main.js
@@ -1,44 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    
+    
     $('#menu').hide()
-    $('#toggle').on('click',function(){
+    $('#toggle').on('click', function () {
         $('#navbar').toggleClass('active')
     })
 
-    
-    $('#search').click(function(){
-        if ($('#navbar').hasClass ('active')) {
-        $('#navbar').removeClass('active');
+
+
+    $('#search').click(function () {
+        if ($('#navbar').hasClass('active')) {
+            $('#navbar').removeClass('active');
         }
     })
-    
+
 })
-$(document).ready(function(){
-    let i=0;
-    $("#slideDown").click(function(){
-      $("#menu").show('slow');
+var title
+$(document).ready(function () {
+    let i = 0;
+    $("#slideDown").click(function () {
+        $("#menu").show('slow');
+        
+    });
+    
+
+    $("#menu").mouseleave(function () {
+        $(this).hide("slow");
     });
 
-    $("#menu").mouseleave(function(){
-        $(this).hide("slow");
-      });
-    
-      $('.uil-plus').click(function () {
-          $(this).prev().text((i+=1).toString())
-          
-      })
-     
-      $('.uil-minus').click(function () {
-        
-        let num =parseInt( $(this).next().text() )
-        if (num>0) {
+    $('.uil-plus').click(function () {
+        $(this).prev().text((i += 1).toString())
+
+    })
+
+    $('.uil-minus').click(function () {
+
+        let num = parseInt($(this).next().text())
+        if (num > 0) {
             num--
             $(this).next().text(num) // read again and prepare for 1 minus again
         }
-        else{
+        else {
             $(this).next().text(0)
         }
-        
-        
+
+
     })
-  });
+
+    
+});
 
